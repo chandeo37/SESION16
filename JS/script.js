59ac6f1a84491e9b2203a4e526ea5e8910c41f6c
chandeo37 <chandeo@gmail.com>
2024-08-18 19:40:06 -0400

    Funcionalidad en JavaScript agregada

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,42 @@
+
+const formulario = document.querySelector("#frmDatos");
+console.log(formulario)
+
+
+formulario.addEventListener("submit", validaFormulario)
+
+function validaFormulario(e){
+e.preventDefault();
+  //console.log('validando')
+  const nombre1 = document.querySelector("#nombre").value;
+  //const mensaje1 = document.querySelector("#mensaje").value;
+  //const correo1 = document.getElementById("#correo")
+  console.log(nombre1)
+  //console.log(correo1)
+
+  const respuesta = document.getElementById("resumenNombre")
+ // const respuesta2 = document.getElementById("resumenMensaje")
+  respuesta.textContent =`Nombre: ${nombre1}`
+  //respuesta2.textContent = `Mensaje: ${mensaje1}`
+  //console.log(nombre1)
+
+
+}
+function guardarDatos(){
+  let email1 = document.getElementById('email').value
+  let mensaje1 = document.getElementById('mensaje').value
+  console.log(email1)
+  console.log(mensaje1)
+
+  const respuestaEmail =document.getElementById("resumenCorreo")
+  respuestaEmail.textContent = `Email: ${email1}`
+
+  const respuestaMensaje = document.getElementById("resumenMensaje")
+  respuestaMensaje.textContent = `Mensaje: ${mensaje1}`
+  
+}
+
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
@@ -16,4 +55,13 @@
         form.classList.add('was-validated')
       }, false)
     })
+
+    
+
   })()
+
+
+
+ 
+
+ 
